02efa01f8e24e4e7aa64250aa9b210989267492f
Daniela Gordas <daniela.gordas@gmail.com>
2020-05-26 16:50:05 +0200

    added styling to show page

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,12 @@
 class LocationsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
 
   def index
     @locations = Location.all
+  end
+
+  def show
+    @location = Location.find(params[:id])
   end
 
   def new
