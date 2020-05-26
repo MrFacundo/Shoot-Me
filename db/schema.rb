59855271caa3662f83034d87deed866ae21caf73
Daniela Gordas <daniela.gordas@gmail.com>
2020-05-26 13:52:03 +0200

    implemented locations-controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_152838) do
+ActiveRecord::Schema.define(version: 2020_05_26_105149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_05_25_152838) do
     t.string "address"
     t.text "description"
     t.integer "daily_fee"
-    t.string "type"
+    t.string "style"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
