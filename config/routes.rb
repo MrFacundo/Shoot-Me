754ce8e6f0e4a72c88c6cb62156e68d765cc9b2c
Beatrice Kern <beatrice.kern23@gmail.com>
2020-05-26 18:06:25 +0200

    booking new & create

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :bookings, only: [ :new, :create ]
-
-  resources :locations, only: [:index, :show, :new, :create]
+  resources :locations, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [ :new, :create ]
+  end
 end
