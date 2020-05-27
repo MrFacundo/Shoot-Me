67065c13d0d8c05066ed14d93123badd7bee0960
Daniela Gordas <daniela.gordas@gmail.com>
2020-05-27 14:27:45 +0200

    implemented geocoding on the index page

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,7 +2,13 @@ class LocationsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show, :index]
 
   def index
-    @locations = Location.all
+    @locations = Location.geocoded
+    @markers = @locations.map do |location|
+      {
+        lat: location.latitude,
+        lng: location.longitude
+      }
+    end
   end
 
   def show
