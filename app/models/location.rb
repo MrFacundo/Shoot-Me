740c1c982dbbf133825462acb8c856493aba7ed1
Juliane Schremer <juliane.schremer@gmail.com>
2020-05-27 12:44:00 +0200

    added photos to location model, controller and new-form

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,7 @@
 class Location < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_many_attached :photos
   TYPE = ['Studio', 'Flat', 'Warehouse', 'Theatre', 'Boat', 'Outdoor', 'Factory']
 
   validates :name, :address, :description, :daily_fee, :style, presence: true
