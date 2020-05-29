2c90647470e6ce02483ce7cb2142d909c26c60da
Facundo Troitero <ftroitero@gmail.com>
2020-05-29 12:08:34 +0200

    implemented total price in bookings show page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,7 +9,7 @@ class BookingsController < ApplicationController
     @location = Location.find(params[:location_id])
     @booking.location = @location
     @booking.user = current_user
-    @booking.total_price = t_price (@booking)
+    @booking.total_price = t_price(@booking)
     if @booking.save
       redirect_to bookings_path, notice: 'Booking was successfully created.'
         #path needs to be updated to redirect to booking show
@@ -43,6 +43,8 @@ class BookingsController < ApplicationController
   def update
     @booking = Booking.find(params[:id])
     @booking.update(booking_params)
+    @booking.total_price = t_price(@booking)
+    @booking.save
     redirect_to bookings_path
   end
 
@@ -53,12 +55,7 @@ class BookingsController < ApplicationController
   end
 
   def t_price(booking)
-    total = (@booking.end_date - @booking.start_date).to_i) * @booking.location.daily_fee
+    total = (@booking.end_date - @booking.start_date).to_i * @booking.location.daily_fee
     total
   end
 end
-
-
-c.end_date - c.start_date
-
-(c.end_date - c.start_date).to_i) * c.location.daily_fee
